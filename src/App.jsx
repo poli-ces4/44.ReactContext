3280e6cc91b8c77ffc239721b15f4d4fc79bc923
Giovanny Vargas <yovaz05@gmail.com>
2024-04-30 19:00:55 -0500

    clase 30/04/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DashboardPage from './pages/DashboardPage';
+import UsersPage from './pages/UsersPage';
+import UserPage from './pages/UserPage';
+import NotFound from './pages/NotFound';
+import Menu from './components/Menu';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<>
+			<BrowserRouter>
+				<Menu />
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/dashboard' element={<DashboardPage />} />
+					<Route path='/users' element={<UsersPage />} />
+					<Route path='/user' element={<UserPage />} />
+					<Route path='/*' element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 };
 
 export default App;
